92c152516100bdf581b7dbdbd415073341550421
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-09-01 11:40:39 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,23 @@
 require_relative 'config/environment'
-
+require 'pry'
 class App < Sinatra::Base
+
+    get '/' do
+        erb :index
+    end
+
+    get '/new' do
+       erb :create_puppy 
+    end
+
+    post '/puppy' do
+        # binding.pry
+        @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+        
+        erb :display_puppy
+    end
+
+
+
 
 end
